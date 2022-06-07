0714134f003aea46d4f01cebbae2cd9ff754e39c
Lakremon <self@lakremon.ru>
2022-06-07 19:38:11 +0700

    MetaMask integration draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PlaceComponent } from './place/place.component';
+import { UploaderComponent } from './uploader/uploader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaceComponent,
+    UploaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/place/place.component.ts
@@ -0,0 +1,189 @@
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Map, View} from "ol";
+import {Tile} from "ol/layer";
+import {XYZ} from "ol/source";
+import {fromLonLat} from "ol/proj";
+import {ethers} from "ethers";
+
+@Component({
+  selector: 'app-place',
+  templateUrl: './place.component.html',
+  styleUrls: ['./place.component.sass']
+})
+export class PlaceComponent implements OnInit, AfterViewInit {
+
+  map!: Map;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new Tile({
+          source: new XYZ({
+            attributions: 'CriptoPlace:© 2022',
+            url:
+              'http://crypto-place.local/tiles/{z}x{y}x{x}.png',
+            maxZoom: 26,
+            projection: 'EPSG:4326',
+            tileSize: 128, // the tile size supported by the ArcGIS tile service
+            maxResolution: 180 / 512, // Esri's tile grid fits 180 degrees on one 512 px tile
+            wrapX: false,
+          }),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([0, 0]),
+        minZoom: 22,
+        maxZoom: 28,
+        zoom: 25
+      })
+    });
+    this.map.on('click', function (e) {
+      console.log(e)
+      // var iconFeatureA = map.getFeaturesAtPixel(e.pixel);
+      // if (iconFeatureA !== null) {
+      //   var adres = iconFeatureA[0].get("adres");
+      //   alert(adres);
+      //   e.preventDefault(); // avoid bubbling
+      // }
+    });
+
+    // let web3 = new Web3('ws://localhost:8546');
+
+    //@ts-ignore
+    console.log(typeof window.ethereum)
+    //@ts-ignore
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        //@ts-ignore
+        ethereum.request({method: "eth_requestAccounts"});
+      } catch (error) {
+        console.log(error);
+        alert('Unknown error');
+      }
+      //@ts-ignore
+      ethereum.request({method: "eth_accounts"}).then(accounts => {
+        console.log(accounts)
+        const contractAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+        const abi = [
+          {
+            inputs: [
+              {
+                internalType: "int256",
+                name: "x",
+                type: "int256",
+              },
+              {
+                internalType: "int256",
+                name: "y",
+                type: "int256",
+              },
+              {
+                internalType: "string",
+                name: "image",
+                type: "string",
+              }
+            ],
+            name: "updateTile",
+            outputs: [],
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+          // {
+          //   inputs: [
+          //     {
+          //       internalType: "string",
+          //       name: "",
+          //       type: "string",
+          //     },
+          //   ],
+          //   name: "nameToFavoriteNumber",
+          //   outputs: [
+          //     {
+          //       internalType: "uint256",
+          //       name: "",
+          //       type: "uint256",
+          //     },
+          //   ],
+          //   stateMutability: "view",
+          //   type: "function",
+          // },
+          // {
+          //   inputs: [
+          //     {
+          //       internalType: "uint256",
+          //       name: "",
+          //       type: "uint256",
+          //     },
+          //   ],
+          //   name: "people",
+          //   outputs: [
+          //     {
+          //       internalType: "uint256",
+          //       name: "favoriteNumber",
+          //       type: "uint256",
+          //     },
+          //     {
+          //       internalType: "string",
+          //       name: "name",
+          //       type: "string",
+          //     },
+          //   ],
+          //   stateMutability: "view",
+          //   type: "function",
+          // },
+          // {
+          //   inputs: [],
+          //   name: "retrieve",
+          //   outputs: [
+          //     {
+          //       internalType: "uint256",
+          //       name: "",
+          //       type: "uint256",
+          //     },
+          //   ],
+          //   stateMutability: "view",
+          //   type: "function",
+          // },
+          // {
+          //   inputs: [
+          //     {
+          //       internalType: "uint256",
+          //       name: "_favoriteNumber",
+          //       type: "uint256",
+          //     },
+          //   ],
+          //   name: "store",
+          //   outputs: [],
+          //   stateMutability: "nonpayable",
+          //   type: "function",
+          // },
+        ];
+        //@ts-ignore
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        console.log(signer)
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        console.log(contract)
+        try {
+          console.log(contract.updateTile(1,1,'sssssss'));
+        } catch (error) {
+          console.log(error);
+        }
+
+
+      });
+    } else {
+      alert("This Web3 application \r\nUse MetaMask extension to use the application features");
+    }
+
+  }
+}
