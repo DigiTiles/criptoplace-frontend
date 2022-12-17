5352d9594e98445f6d749bb60747319fd2c1ceb0
evgeniydukhanov <dukhanoves@gmail.com>
2022-12-17 17:34:34 +0700

    changed dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { PlaceComponent } from './place/place.component';
 import { UploaderComponent } from './uploader/uploader.component';
